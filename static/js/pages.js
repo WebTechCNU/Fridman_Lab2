03059037e55fc51bac57a23c2dd6ebc9f49fc0d9
YuriiFridman <102798350+YuriiFridman@users.noreply.github.com>
2023-03-08 17:57:23 +0200

    Add files via upload

--- static/js/pages.js
+++ static/js/pages.js
@@ -4,42 +4,48 @@ const images = [
         src: 'static/images/image1.png',
         alt: 'Товар 1',
         description: 'Насіння кінзи Seedra для посадки в приміщенні та на відкритому повітрі',
-        price: '12.69$'
+        price: '12.69$',
+        classname: 'bundles'
     },
 
     {
         src: 'static/images/image1.png',
         alt: 'Товар 2',
         description: 'Насіння кінзи Seedra для посадки в приміщенні та на відкритому повітрі',
-        price: '12.69$'
+        price: '12.69$',
+        classname: 'herbs'
     },
 
     {
         src: 'static/images/image2.png',
         alt: 'Товар 3',
         description: 'Кукурудза SEEDRA - гібридне насіння Bodacious для внутрішнього саду',
-        price: '12.69$'
+        price: '12.69$',
+        classname: 'vegetables'
     },
 
     {
         src: 'static/images/image2.png',
         alt: 'Товар 4',
         description: 'Кукурудза SEEDRA - гібридне насіння Bodacious для внутрішнього саду',
-        price: '12.69$'
+        price: '12.69$',
+        classname: 'bundles'
     },
 
     {
         src: 'static/images/image3.png',
         alt: 'Товар 5',
         description: 'Насіння шпинату SEEDRA для внутрішнього та відкритого саду',
-        price: '12.69$'
+        price: '12.69$',
+        classname: 'vegetables'
     },
 
     {
         src: 'static/images/image3.png',
         alt: 'Товар 6',
         description: 'Насіння шпинату SEEDRA для внутрішнього та відкритого саду',
-        price: '12.69$'
+        price: '12.69$',
+        classname: 'herbs'
     }
 ];
 
@@ -49,7 +55,8 @@ const showImagesBtn = document.getElementById('show-images-btn');
 // Функція для додавання зображення та опису до контейнеру
 function addImageToContainer(image) {
     const imageCol = document.createElement('div');
-    imageCol.classList.add('col-md-4', 'my-5');
+    imageCol.classList.add('col-md-4', 'my-5', 'float-left', 'box');
+    imageCol.classList.add(image.classname);
 
     const img = document.createElement('img');
     img.src = image.src;
@@ -99,3 +106,74 @@ showImagesBtn.addEventListener('click', function () {
         addImageToContainer(image);
     });
 });
+
+
+setTimeout(function () {
+    // Перевірка, чи користувач вже підписаний на повідомлення
+    if (localStorage.getItem('subscribed') !== 'true') {
+        // Показуємо вікно
+        var subscribeModal = new bootstrap.Modal(document.getElementById('subscribeModal'));
+        subscribeModal.show();
+        // Додаємо обробник події на кнопку "Підписатися"
+        document.getElementById('subscribeButton').addEventListener('click', function () {
+            // Зберігаємо інформацію про підписку у локальному сховищі
+            localStorage.setItem('subscribed', 'true');
+            // Ховаємо вікно
+            subscribeModal.hide();
+            // Показуємо повідомлення про подяку за підписку
+            alert('Дякуємо за підписку!');
+        });
+    }
+}, 10000);
+
+// Фільтрація товару
+
+
+
+document.querySelector('nav.midlbuttom').addEventListener('click', (event)=>{
+   if (event.target.tagName !== 'LI') return false;
+
+   let filterClass = event.target.dataset['f'];
+
+   let filterProducts = document.querySelectorAll('.box');
+
+   Array.from(filterProducts).forEach(elem => {
+       elem.classList.remove('hide');
+        if (!elem.classList.contains(filterClass) && filterClass !== 'all'){
+            elem.classList.add('hide');
+        }
+   });
+});
+
+
+
+const modal = document.getElementById('modal');
+const modalContent = document.getElementById('modal-content');
+const closeBtn = document.getElementById('close-btn');
+const timer = document.getElementById('timer');
+
+// Показуємо модальне вікно після певного часу перебування на сторінці
+setTimeout(function() {
+    modal.style.display = 'block';
+}, 5000);
+
+// Змінюємо текст таймера кожну секунду
+let count = 10;
+const interval = setInterval(function() {
+    count--;
+    if (count >= 0) {
+        timer.innerHTML = 'Зачекайте ' + count + ' секунд, щоб закрити рекламу';
+    } else {
+        clearInterval(interval);
+        timer.innerHTML = 'Ви можете закрити рекламу';
+        closeBtn.classList.add('active');
+    }
+}, 1000);
+
+// Закриваємо модальне вікно при кліку на кнопку закриття
+closeBtn.addEventListener('click', function() {
+    if (closeBtn.classList.contains('active')) {
+        modal.style.display = 'none';
+    }
+});
+
